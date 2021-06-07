6f31c70e12387821c226705d4a8b5400e3594a3d
Harry Hodgkins <harry.hodgkins@kellogg.northwestern.edu>
2021-06-07 15:34:48 -0500

    adding additional front end, starting backend

--- index.js
+++ index.js
@@ -1,11 +1,90 @@
+
 firebase.auth().onAuthStateChanged(async function(user) {
   if (user) {
-    // Signed in
-    console.log('signed in')
+    // signed in
+    console.log ('user logged in: ' , user)
+
+    // let db = firebase.firestore()
+    // db.collection('users').doc(user.uid).set ({
+    //   name: user.displayName,
+    //   email: user.email,
+    // })
+
+    const logout = document.querySelector('#sign-in-or-sign-out');
+    logout.addEventListener('click', (event) => {
+      event.preventDefault();
+      firebase.auth().signOut()
+      console.log("logged out");
+      document.location.href = 'index.html'
+    })
+
+    // show welcome message
+    let name = firebase.auth().currentUser.displayName
+    console.log(name);
+    document.querySelector(".welcome-message").insertAdjacentHTML("beforeend", `
+      <h1 class="welcome-message text-3xl md:text-3xl p-8 text-white">Welcome, ${name}!</h1>
+      `)
+
+    // show navigation instructions
+
+    // Build the URL for our posts API
+    let url = `/.netlify/functions/nft_listing`
+  
+    // Fetch the url, wait for a response, store the response in memory
+    let response = await fetch(url)
+  
+    // Ask for the json-formatted data from the response, wait for the data, store it in memory
+    let json = await response.json()
+  
+    // Write the json-formatted data to the console in Chrome
+    console.log(json)
+  
+    // Grab a reference to the element with class name "posts" in memory
+    let nftsDiv = document.querySelector(`.nft_listing`)
+  
+    // Loop through the JSON data, for each Object representing a nft:
+    for (let i=0; i < json.length; i++) {
+      // Store each object ("nft") in memory
+      let nft = json[i]
+  
+  
+      // Create some markup using the nft data, insert into the "nfts" element
+
+
+      nftsDiv.insertAdjacentHTML(`beforeend`, `
+        <div class="md:mt-16 mt-8">
+          <div class="md:mx-0 mx-4 mt-8">
+            <span class="font-bold text-xl">${nft.nftName}</span>
+          </div>
+
+          <div class="md:mx-0 mx-4 mt-8">
+            <span class="font-bold text-xl">${nft.nftCreator}</span>
+          </div>
+
+          <div class="md:mx-0 mx-4 mt-8">
+             <span class="font-bold text-xl">${nft.nftOwner}</span>
+          </div>
+
+          <div class="md:mx-0 mx-4 mt-8">
+            <span class="font-bold text-xl">${nft.sellPrice}</span>
+         </div>
+      
+          <div class="my-8">
+            <img src="${nft.imageUrl}" class="w-full">
+      `)
+    }
+
+
   } else {
-    // Signed out
+    // signed out
     console.log('signed out')
 
+    // hide elements when signed out
+    document.querySelector('.navigation').classList.add('hidden')
+    document.querySelector('.page-title').classList.add('hidden')
+    document.querySelector('.page-content').classList.add('hidden')
+
+    // user is not logged-in, so show login
     // Initializes FirebaseUI Auth
     let ui = new firebaseui.auth.AuthUI(firebase.auth())
 
@@ -14,10 +93,10 @@ firebase.auth().onAuthStateChanged(async function(user) {
       signInOptions: [
         firebase.auth.EmailAuthProvider.PROVIDER_ID
       ],
-      signInSuccessUrl: 'index.html'
+      signInSuccessUrl: `index.html` // where to go after we're done signing up/in
     }
 
     // Starts FirebaseUI Auth
-    ui.start('.sign-in-or-sign-out', authUIConfig)
+    ui.start(`.sign-in-or-sign-out`, authUIConfig)
   }
-})
+  })
