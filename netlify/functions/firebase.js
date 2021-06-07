954e2f9e502dc718991d16129a13a6474d69c7ed
ally-kirst-kellogg <ally.kirst@kellogg.northwestern.edu>
2021-06-06 22:39:37 -0500

    added firebase configs

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,16 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyBlxGQB0zkHA6PtRFQcjb1Ek0z2h2TK_bU",
+  authDomain: "nft-marketplace-a02be.firebaseapp.com",
+  databaseURL: "https://nft-marketplace-a02be-default-rtdb.firebaseio.com",
+  projectId: "nft-marketplace-a02be",
+  storageBucket: "nft-marketplace-a02be.appspot.com",
+  messagingSenderId: "833934020342",
+  appId: "1:833934020342:web:d9a482926f0a4c16c326d7",
+  measurementId: "G-4DX867X47E"
+}
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
